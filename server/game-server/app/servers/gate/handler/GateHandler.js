6a4d65ae99bfbe3b17e034b0b912da64b89b44a6
sss316 <sss316@163.com>
2017-04-12 03:00:25 +0800

    优化代码

---
+++ server/game-server/app/servers/gate/handler/GateHandler.js
@@ -0,0 +1,42 @@
+var cc = require("../../..//game/util/cc");
+
+module.exports = function(app) {
+    return new Handler(app);
+};
+
+var Handler = cc.Class.extend({
+    _baseUid:1,
+    ctor:function (app) {
+        this.app = app;
+    },
+
+
+    /**
+     * Gate handler that dispatch user to connectors.
+     *
+     * @param {Object} msg message from client
+     * @param {Object} session
+     * @param {Function} next next stemp callback
+     *
+     */
+    queryEntry : function(msg, session, next) {
+        // get all connectors
+        var connectors = this.app.getServersByType('conn');
+        if(!connectors || connectors.length === 0) {
+            next(null, {
+                code: 500
+            });
+            return;
+        }
+
+        // select conn
+        var res = connectors[Math.floor(Math.random()*connectors.length)];
+        next(null, {
+            code: 200,
+            host: res.host,
+            port: res.clientPort,
+            uid: 'u'+this._baseUid
+        });
+        this._baseUid++;
+    },
+});
