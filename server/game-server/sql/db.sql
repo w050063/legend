a0738c52c94380a81340eda578a9ebb10b71a1f5
sss316 <sss316@163.com>
2017-12-12 00:27:55 +0800

    设计更改

---
+++ server/game-server/sql/db.sql
@@ -0,0 +1,71 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50624
+Source Host           : localhost:3306
+Source Database       : nodejs
+
+Target Server Type    : MYSQL
+Target Server Version : 50624
+File Encoding         : 65001
+
+Date: 2016-12-30 22:47:53
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `t_accounts`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_accounts`;
+CREATE TABLE `t_accounts` (
+  `id` varchar(16) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `create_time` varchar(16) NOT NULL DEFAULT '0',
+  `last_time` varchar(16) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `t_roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_roles`;
+CREATE TABLE `t_roles` (
+  `id` varchar(16) NOT NULL,
+  `map_id` varchar(4) NOT NULL,
+  `x` int(4) NOT NULL,
+  `y` int(4) NOT NULL,
+  `type` varchar(16) NOT NULL,
+  `camp` int(1) NOT NULL,
+  `sex` int(1) NOT NULL,
+  `direction` int(1) NOT NULL,
+  `level` int(4) NOT NULL,
+  `exp` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `t_items`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_items`;
+CREATE TABLE `t_items` (
+  `id` varchar(16) NOT NULL,
+  `mid` varchar(16) NOT NULL,
+  `owner` varchar(16) NOT NULL,
+  `puton` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `t_chats`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_chats`;
+CREATE TABLE `t_chats` (
+  `id` int(16) unsigned NOT NULL AUTO_INCREMENT,
+  `aid` varchar(16) NOT NULL,
+  `chat` varchar(4096) NOT NULL,
+  `chat_time` varchar(16) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
